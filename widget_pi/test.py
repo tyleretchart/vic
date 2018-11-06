986411f247990c5b64c8b3566a8f318a193252e3
Tyler Etchart <tyler.etchart@gmail.com>
2018-11-05 21:57:36 -0700

    updated test for server

--- widget_pi/test.py
+++ widget_pi/test.py
@@ -1,4 +1,10 @@
 import requests
 
 
-r = requests.post(url='http://0.0.0.0:8000/start', data={"code": "x = 5; print(x)"})
+# code = "import RPi.GPIO as GPIO\nGPIO.setmode(GPIO.BCM)\nGPIO.output(18, GPIO.HIGH)"
+# code = "led = LED(18)\nled.on()"
+# code = "led = LED(18)\nled.on()\nled.off()"
+code = "led = LED(18)\nled.on()\ntime.sleep(1)\nled.off()"
+# code = "x = 5; print(x)"
+print(code)
+r = requests.post(url='http://elderberry:8000/start', data={"code": code})
